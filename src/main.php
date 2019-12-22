db607a08295630d1c040f363974af21c936eb2b2
Marius Ghita <mhitza@gmail.com>
2019-12-22 03:37:26 +0200

    Revert "subgraphs used for "rendering" tables"
    This reverts commit 1e3b16ded9b0a4ecb795a1e279f85782c41462f7.

--- src/main.php
+++ src/main.php
@@ -1,7 +1,6 @@
 <?php
 
 $print = true;
-$subgraphs = true;
 
 use Doctrine\DBAL\Configuration;
 use Doctrine\DBAL\DriverManager;
@@ -31,53 +30,27 @@ while ($row = $statement->fetch()) {
     $tableName = current($row);
     $tables[$tableName] = [];
 
-    if ($print && !$subgraphs) {
+    if ($print) {
         print "\t$tableName;\n";
     }
 }
 
-$clusterIndex = 0;
-$collected = [];
 foreach ($tables as $tableName => $value) {
     $createStatement = $connection->query(sprintf("SHOW CREATE TABLE %s", $tableName));
     while ($row_ = $createStatement->fetch()) {
         $description = $row_['Create Table'];
-
-        if ($subgraphs) {
-            printf("\tsubgraph cluster_%d {\n", $clusterIndex++);
-            printf("\t\tlabel=\"%s\";\n", $tableName);
-        }
-
         $lines = explode("\n", $description);
         foreach ($lines as $line) {
-            if ($subgraphs && preg_match('/^\s+`(.*?)`/', $line, $matches)) {
-                $column = $matches[1];
-                printf("\t\t\"%s.%s\";\n", $tableName, $column);
-            }
             if (preg_match('/FOREIGN KEY \(.*\) REFERENCES .*?\(.*\)/', $line, $matches)) {
                 $relationship = str_replace(['FOREIGN KEY ', 'REFERENCES ', '(', ')', '`'], '', $matches[0]);
 
-                if ($print && !$subgraphs) {
+                if ($print) {
                     $parts = explode(' ', $relationship);
                     printf("\t%s -> %s [label=\"%s\"]; \n", $tableName, $parts[1], $parts[0]);
                 }
-
-                if ($subgraphs) {
-                    $parts = explode(' ', $relationship);
-                    $collected[] = sprintf("\t\"%s.%s\" -> \"%s.%s\";\n", $tableName, $parts[0], $parts[1], $parts[2]);
-                }
             }
-        }
 
-        if ($subgraphs) {
-            printf("\t}\n");
         }
-    }
-}
-
-if ($subgraphs) {
-    foreach ($collected as $relation) {
-        print $relation;
     }
 }
 
